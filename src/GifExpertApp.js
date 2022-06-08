f02283ad8efeaf27f0bb73b5f9a31accc63c96a0
EdwinTorrado15 <edwintorrado15@gmail.com>
2022-06-08 15:24:26 -0500

    Modificacion

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,7 +3,7 @@ import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
 const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["Dragon ball z"]);
+  const [categories, setCategories] = useState([" "]);
 
   return (
     <Fragment>
